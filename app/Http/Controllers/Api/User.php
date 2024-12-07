3450ef88b45c9b05acd002610675df0cc4d8a91d
lee <leem79763@gmail.com>
2024-12-07 18:43:38 +0800

    测试

--- app/Http/Controllers/Api/User.php
+++ app/Http/Controllers/Api/User.php
@@ -11,15 +11,18 @@ class User extends Controller
 {
     public function index(Request $request)
     {
-        $userName = $request->post('username');
-        $password = $request->post('password');
-        $res = UserModel::factory()
-            ->newModel()
-            ->newQuery()
-            ->where('name', $userName)
-            ->where('password', $password)
-            ->first();
+//        $userName = $request->post('username');
+//        $password = $request->post('password');
+//        $res = UserModel::factory()
+//            ->newModel()
+//            ->newQuery()
+//            ->where('name', $userName)
+//            ->where('password', $password)
+//            ->first();
 
+        $res = [
+            "success" => "你好",
+        ];
         $domain = $request->getHost(); // 获取当前请求的域名
 
         // 创建一个 Cookie 实例
